e68b60a1b163d399a4ce8be037aaa06cd4ed38a8
Leviton Lima Carvalho <carvalholevis@icloud.com>
2026-04-27 15:05:56 +0100

    feat: fix some bugs in the model

--- src/data.py
+++ src/data.py
@@ -15,7 +15,8 @@ def split_data(df, target_col='is_ultra', test_size=0.4, valid_size=0.5, random_
     X_train, X_rest, y_train, y_rest = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
     # Split the rest equally into validation and test
-    X_valid, X_test, y_valid, y_test = train_test_split(X_rest, y_rest, test_size=test_size, random_state=random_state)
+    X_valid, X_test, y_valid, y_test = train_test_split(X_rest, y_rest, test_size=valid_size, random_state=random_state)
+
 
     return X_train, X_valid, X_test, y_train, y_valid, y_test
 
